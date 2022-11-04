ac3a945357bfa115e66831387d30c124b6658562
GURAMI KIKNADZE <gurikiknadze@gmail.com>
2022-11-05 00:58:40 +0400

    fin of third

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 1) show rating by *
 
-function showRating(rating) {
+/* function showRating(rating) {
     let ratings = "";
         for (let i = 0; i < Math.floor(rating); ++i) {
             ratings = ratings + "*"
@@ -9,8 +9,58 @@ function showRating(rating) {
             }
         }
         if (rating % Math.floor(rating) !== 0) {
-            console.log('.')
+            ratings = ratings + " ."
+        }
         return ratings; 
     }
+
+console.log(showRating(4.5)); */
+
+
+/* function showRating(rating) {
+    let ratings = "";
+        for (let i = 0; i < Math.floor(rating); ++i) {
+            ratings = ratings + "*"
+            if (i !== Math.floor(rating) - 1) {
+                ratings = ratings + " "
+            }
+        }
+
+ */
+        function showRating(rating) {
+            let ratings = "";
+                for (let i = 0; i < Math.floor(rating); ++i) {
+                    ratings = ratings + "*"
+                    if (i !== Math.floor(rating) - 1) {
+                        ratings = ratings + " "
+                    }
+                }
+                if (rating % Math.floor(rating) !== 0) {
+                    ratings = ratings + " ."
+                }
+                return ratings; 
+            }
+        
+        console.log(showRating(4.5));
+
+function showRating(rating) {
+    // initialise an empty string
+    let ratings = ""; 
+    // loop a star for every iteration
+    for (let i = 0; i < Math.floor(rating); ++i) {
+        ratings = ratings + "*"
+    // if its not the last iteration add a space
+        if (i !== Math.floor(rating) - 1) {
+        ratings = ratings + " "
+    }
 }
-console.log(showRating(4.5));
+if (rating % Math.floor(rating) !== 0) {
+    ratings = ratings + " ."
+}
+    // if the number is not an integer 
+    //add a full stop
+    // return
+    return ratings;
+}
+
+console.log(showRating(4.5))
