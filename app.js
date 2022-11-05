47b22f2e1169d4b7c48a9e4f8137cd4afce333f2
GURAMI KIKNADZE <gurikiknadze@gmail.com>
2022-11-05 13:54:11 +0400

    promises

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ console.log(showRating(4.5)); */
         }
 
  */
-        function showRating(rating) {
+        /* function showRating(rating) {
             let ratings = "";
                 for (let i = 0; i < Math.floor(rating); ++i) {
                     ratings = ratings + "*"
@@ -41,9 +41,9 @@ console.log(showRating(4.5)); */
                 return ratings; 
             }
         
-        console.log(showRating(4.5));
+        console.log(showRating(4.5)); */
 
-function showRating(rating) {
+/* function showRating(rating) {
     // initialise an empty string
     let ratings = ""; 
     // loop a star for every iteration
@@ -63,4 +63,33 @@ if (rating % Math.floor(rating) !== 0) {
     return ratings;
 }
 
-console.log(showRating(4.5))
+console.log(showRating(4.5)) */
+
+///////////////////////////////////
+// 2) sort low to high
+
+/* function sortLowToHigh(arr) {
+
+        return arr.sort((a, b) => a - b);
+    
+}
+console.log(sortLowToHigh([5, 8, 10, 11, 3, 1])) 
+*/
+
+
+/////////////////////////////////////
+// 3) sort by highest to lowest price
+
+/* function sortHighToLow(number) {
+    return number.sort((a, b) => b.price - a.price);
+    //tell JS what we need property
+}
+
+console.log(sortHighToLow([
+    { id: 1, price: 50},
+    { id: 2, price: 0},
+    { id: 3, price: 400},
+    { id: 4, price: 100},
+])); */
+
+
